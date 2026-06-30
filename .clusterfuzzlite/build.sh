dbba136d73a155deb8bbc17878c69cce259a226e
a-micable <a-micable@users.noreply.github.com>
2026-06-30 15:55:09 +0300

    Fix ClusterFuzzLite offline build config

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -8,17 +8,24 @@ fi
 
 mkdir -p "${OUT}"
 
-cmake -S . -B build-cflite \
-  -DCMAKE_CXX_COMPILER="${CXX:-clang++}" \
-  -DCMAKE_C_COMPILER="${CC:-clang}" \
+BUILD_DIR="${BUILD_DIR:-/tmp/bse-cflite-build}"
+STANDALONE_FUZZERS=OFF
+if [ -z "${LIB_FUZZING_ENGINE:-}" ]; then
+  STANDALONE_FUZZERS=ON
+fi
+
+cmake -S . -B "${BUILD_DIR}" \
+  -DCMAKE_CXX_COMPILER="${CXX:-c++}" \
+  -DCMAKE_C_COMPILER="${CC:-cc}" \
   -DBSE_BUILD_TESTS=OFF \
   -DBSE_BUILD_EXAMPLES=OFF \
   -DBSE_BUILD_BENCHMARKS=OFF \
   -DBSE_BUILD_FUZZERS=ON \
+  -DBSE_STANDALONE_FUZZERS="${STANDALONE_FUZZERS}" \
   -DCMAKE_BUILD_TYPE=RelWithDebInfo
 
-cmake --build build-cflite --parallel
+cmake --build "${BUILD_DIR}" --parallel
 
 for target in mesh_fuzzer scene_fuzzer serializer_fuzzer animation_fuzzer compression_fuzzer validator_fuzzer; do
-  install -m 0755 "build-cflite/${target}" "${OUT}/${target}"
+  install -m 0755 "${BUILD_DIR}/${target}" "${OUT}/${target}"
 done
